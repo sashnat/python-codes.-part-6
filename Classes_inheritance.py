25784cfdce353a3c42c7e1e4b2093387c71fa3c7
Nataliya Sashnikova <sashnat@yahoo.com>
2018-04-29 22:48:58 +0300

    Update Classes_inheritance.py

--- Classes_inheritance.py
+++ Classes_inheritance.py
@@ -15,6 +15,7 @@ class Oval(Figure):
          self.radius = r
          self.ox = x
          self.oy = y
+         super().__init__()
      def out(self):
          print (self.radius,self.ox,self.oy)
 
@@ -23,6 +24,7 @@ class Square(Figure):
      def __init__(self,w,h):
          self.widht = w
          self.height = h
+         super().__init__()
      def howsides(self,n):
           self.sides = n
           if not self.sides == 4:
@@ -46,3 +48,13 @@ s.howsides(4)
 s.outsides()
 s.changecolor("grey")
 print(s.chcolor, s.widht, s.height)
+
+'''
+-------------------------------output--------------------------
+red
+5 0 0
+black 5
+It is a sguare
+4
+grey 8 8
+'''
